e5bd6673a6d581c02159c3f5a3eabc0e14d8dbfe
tyeung <tyeung@walmartlabs.com>
2016-06-02 15:40:50 -0700

    Init commit

---
+++ journal/GridViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GridViewController.swift
+//  journal
+//
+//  Created by Tony Yeung on 6/2/16.
+//  Copyright © 2016 Tony Yeung. All rights reserved.
+//
+
+import UIKit
+
+class GridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onBackButtonPressed(sender: AnyObject) {
+        print("back btn in grid view pressed")
+        navigationController!.popViewControllerAnimated(true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
